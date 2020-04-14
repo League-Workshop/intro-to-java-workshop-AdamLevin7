261507aa61db56243e582db9e729b49f1a454af0
adam <adam@192.168.86.234>
2020-04-14 14:51:12 -0700

    Robot draws some cool shapes - Day 2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,6 +8,15 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot bob = new Robot();
+	bob.setWindowColor(255, 100, 255);
+	bob.penDown();
+	bob.setPenColor(0, 255, 255);
+	bob.setSpeed(100);
+	for (int i = 0; i < 6; i++) {
+		bob.move(100);
+		bob.turn(360/6);
+	}
 	
 		
 		
